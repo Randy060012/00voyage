1ca932090afe280e7c06f23e2985a53fe6ece659
Randy060012 <randy061200@gmail.com>
2024-12-30 23:06:59 +0100

    First push here

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('client.tour.index');
+})->name('home');
+
+Route::get('/listes', function () {
+    return view('client.tour.list');
+})->name('liste');
+
+
+Route::get('/details', function () {
+    return view('client.tour.show');
+})->name('detail');
+
+Route::get('/contact', function () {
+    return view('client.tour.contact');
+})->name('contact');
+
+Route::get('/help', function () {
+    return view('client.tour.help');
+})->name('help');
+
+Route::get('/about', function () {
+    return view('client.tour.about');
+})->name('about');
